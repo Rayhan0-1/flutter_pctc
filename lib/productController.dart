2eb8733ffbb8dd49a107c350b3a6139b0d4079ab
Rayhan0-1 <rayhanahmedshourov001@gmail.com>
2025-07-15 02:46:58 +0600

    edit_delete

--- lib/productController.dart
+++ lib/productController.dart
@@ -1,18 +1,19 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:rest_api_ui_design/utils/urls.dart';
+import 'model/product.dart';
 
 
 class productController{
-  List products = [];
+  List<Data> products = [];
 
   Future<void>fetchProducts() async{
     final response = await http.get(Uri.parse(Urls.readProduct));
     print(response.statusCode);
     if(response.statusCode == 200){
       final data= jsonDecode(response.body);
-      products = data['data'];
+      productModel model = productModel.fromJson(data);
+      products = model.data ?? [];
     }
 }
 
@@ -31,6 +32,34 @@ class productController{
     print(response.statusCode);
     if(response.statusCode == 201){
       fetchProducts();
+    }
+  }
+
+  Future<void>updateProducts(String id, String name, String img, int qty, int price, int totalPrice, ) async{
+    final response = await http.post(Uri.parse(Urls.updateProduct(id)),
+        headers: {'Content-Type' : 'application/json'},
+        body: jsonEncode({
+          "ProductName": name,
+          "ProductCode": DateTime.now().microsecondsSinceEpoch,
+          "Img": img,
+          "Qty": qty,
+          "UnitPrice": price,
+          "TotalPrice": totalPrice,
+        })
+    );
+    print(response.statusCode);
+    if(response.statusCode == 200){
+      fetchProducts();
+    }
+  }
+
+  Future<bool>deleteProducts(String id) async{
+    final response = await http.get(Uri.parse(Urls.deleteProduct(id)));
+    if(response.statusCode == 200){
+      return true;
+    }
+    else{
+      return false;
     }
   }
 }
--- lib/restApiUIDesign.dart
+++ lib/restApiUIDesign.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'productController.dart';
+
 class restApiUIDesign extends StatefulWidget {
   const restApiUIDesign({super.key});
 
@@ -8,21 +9,28 @@ class restApiUIDesign extends StatefulWidget {
 }
 
 class _restApiUIDesignState extends State<restApiUIDesign> {
-
   final productController product_Controller = productController();
 
-  void productDialog() {
+  void productDialog({String ? id, String ? name, String ? qty, String ? img, int ? unitPrice, int ? totalPrice}) {
+
+
     TextEditingController productNameController = TextEditingController();
     // TextEditingController productCodeController = TextEditingController();
     TextEditingController productImageController = TextEditingController();
     TextEditingController productQtyController = TextEditingController();
     TextEditingController productUnitPriceController = TextEditingController();
     TextEditingController productTotalPriceController = TextEditingController();
+
+    productNameController.text = name ?? '';
+    productQtyController.text = qty.toString() ?? '';
+    productImageController.text = img ?? '';
+    productUnitPriceController.text = unitPrice.toString() ?? '';
+    productTotalPriceController.text = totalPrice.toString() ?? '';
+
     showDialog(
         context: context,
-        builder: (context) =>
-            AlertDialog(
-              title: Text('Add product'),
+        builder: (context) => AlertDialog(
+              title: Text(id == null ? 'Add product' : 'Update product'),
               content: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
@@ -40,29 +48,51 @@ class _restApiUIDesignState extends State<restApiUIDesign> {
                   ),
                   TextField(
                     controller: productUnitPriceController,
-                    decoration: InputDecoration(
-                        labelText: 'product unit price'),
+                    decoration:
+                        InputDecoration(labelText: 'product unit price'),
                   ),
                   TextField(
                     controller: productTotalPriceController,
                     decoration: InputDecoration(labelText: 'Total price'),
                   ),
-
-                  SizedBox(height: 10,),
+                  SizedBox(
+                    height: 10,
+                  ),
                   Row(
-                    
                     mainAxisSize: MainAxisSize.min,
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      TextButton(onPressed: ()=>
-                          Navigator.pop(context),
+                      TextButton(
+                          onPressed: () => Navigator.pop(context),
                           child: Text('Close')),
                       SizedBox(width: 10),
-                      ElevatedButton(onPressed: (){
-                        product_Controller.createProducts(productNameController.text, productImageController.text, int.parse(productQtyController.text) , int.parse(productUnitPriceController.text), int.parse(productTotalPriceController.text));
-                        fetchData();
-                        Navigator.pop(context);
-                      }, child: Text('Add product'),),
+                      ElevatedButton(
+                        onPressed: () {
+                            if(id == null){
+                              product_Controller.createProducts(
+                                  productNameController.text,
+                                  productImageController.text,
+                                  int.parse(productQtyController.text),
+                                  int.parse(productUnitPriceController.text),
+                                  int.parse(productTotalPriceController.text));
+                            }
+                            else{
+                              product_Controller.updateProducts(id,
+                                  productNameController.text,
+                                  productImageController.text,
+                                  int.parse(productQtyController.text),
+                                  int.parse(productUnitPriceController.text),
+                                  int.parse(productTotalPriceController.text));
+                            }
+
+                          fetchData();
+                          Navigator.pop(context);
+                          setState(() {
+
+                          });
+                        },
+                        child: Text(id == null ? 'Add product' : 'Update product'),
+                      ),
                     ],
                   )
                 ],
@@ -70,9 +100,12 @@ class _restApiUIDesignState extends State<restApiUIDesign> {
             ));
   }
 
-  Future<void>fetchData()async {
+  Future<void> fetchData() async {
     await product_Controller.fetchProducts();
-     print(product_Controller.products.length);
+    print(product_Controller.products.length);
+    setState(() {
+
+    });
   }
 
   @override
@@ -80,9 +113,7 @@ class _restApiUIDesignState extends State<restApiUIDesign> {
     // TODO: implement initState
     super.initState();
     fetchData();
-
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -101,28 +132,63 @@ class _restApiUIDesignState extends State<restApiUIDesign> {
                 // leading: Image.network(product['Img'],
                 //      width: 150, fit: BoxFit.contain,),
                 title: Text(
-                  product['ProductName'], style: TextStyle(fontWeight: FontWeight.bold),),
-                subtitle: Text('price: \$${product['UnitPrice']}| Qty: ${product['Qty']}', style: TextStyle(),),
+                  product.productName.toString(),
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text(
+                  'price: \$${product.unitPrice}| Qty: ${product.qty}',
+                  style: TextStyle(),
+                ),
                 trailing: Row(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    IconButton(onPressed: ()=> productDialog(), icon: Icon(Icons.edit)),
-                    SizedBox(width: 10,),
-                    IconButton(onPressed: () {},
-                        icon: Icon(Icons.delete, color: Colors.red,))
+                    IconButton(
+                        onPressed: () => productDialog(
+                          id: product.sId,
+                          name: product.productName,
+                          img: product.img,
+                          qty: product.qty.toString(),
+                          unitPrice: product.unitPrice,
+                          totalPrice: product.totalPrice,
+                        ),
+                        icon: Icon(Icons.edit)),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    IconButton(
+                        onPressed: () {
+                          product_Controller.deleteProducts(product.sId.toString()).then((value){
+                            if(value){
+                              setState(() {
+                                fetchData();
+                              });
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text('Product deleted'),
+                                  duration: Duration(seconds: 2),
+                                ),
+                              );
+                            }else{
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text('Something wrong, try again'),
+                                  duration: Duration(seconds: 2),
+                                ),
+                              );
+                            }
+                          });
+                        },
+                        icon: Icon(
+                          Icons.delete,
+                          color: Colors.red,
+                        ))
                   ],
                 ),
               ),
             );
-          }
-      ),
-
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: () => productDialog(),
-        child: Icon(Icons.add),),
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
-
-
-
